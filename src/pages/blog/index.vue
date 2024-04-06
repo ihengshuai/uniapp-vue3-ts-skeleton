de70366451a09c2664e603f6c7f1bf307221ac7c
取舍 <wsm_1105@163.com>
2024-04-06 14:58:02 +0800

    feat: 新增webview例子(#0)

---
+++ src/pages/blog/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <web-view
+    :src="websiteURL"
+    @load="onWebviewLoaded"
+  ></web-view>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const websiteURL = ref("https://blog.usword.cn");
+
+function onWebviewLoaded() {
+  console.log("onWebviewLoaded...");
+}
+</script>
